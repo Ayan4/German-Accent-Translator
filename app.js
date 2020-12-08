7d73af82e95fbb781b68168925589430046d9e92
ayan shukla <ayanshukla4@gmail.com>
2020-12-08 17:12:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const translateBtn = document.querySelector('#click');
+const textInput = document.querySelector('#text-input');
+const output = document.querySelector('#output');
+const clearBtn = document.querySelector('#clear');
+
+const url = 'https://api.funtranslations.com/translate/german-accent.json';
+
+const getTranslationURL = (text) =>{
+    return url + '?' + 'text=' + text
+}
+
+const errorHandler = (error) =>{
+    alert(`Cannot leave blank`)
+}
+
+    translateBtn.addEventListener('click', () =>{
+        const inputText = textInput.value
+        fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(data => {
+            const translatedText = data.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler);
+    })
+
+clearBtn.addEventListener('click', () =>{
+    textInput.value = '';
+    output.innerText = '';
+})
